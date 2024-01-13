31021587ca6d7031277b8b73eea7827f3c9e191b
clementcollin99 <clementcollin2@gmail.com>
2024-01-13 17:44:24 +0100

    geom_utils : quick refacto

--- src/geom_utils.py
+++ src/geom_utils.py
@@ -8,24 +8,19 @@ OFFSET = 10
 
 def get_y_parabola(x: float, focus: Point, y_sweep_line: float):
     return list(
-        (x**2 - 2 * focus.x * x + focus.x**2 + focus.y**2 - y_sweep_line**2)
-        / (2 * (focus.y - y_sweep_line))
-    )
+        (x**2 - 2 * focus.x * x + focus.x**2 + focus.y**2 - y_sweep_line**2) /
+        (2 * (focus.y - y_sweep_line)))
 
 
 def get_intersection(breakpoint, y_sweep_line: float, max_y: float = None):
-    i, j = breakpoint.get_left_arc().focus, breakpoint.get_right_arc().focus
-    l = y_sweep_line
-
+    i = breakpoint.get_left_arc().focus
+    j = breakpoint.get_right_arc().focus
     result = Point((np.inf, np.inf))
-    p = i
 
-    a = i.x
-    b = i.y
-    c = j.x
-    d = j.y
-    u = 2 * (b - l)
-    v = 2 * (d - l)
+    s = y_sweep_line
+    p = i
+    u = 2 * (i.y - s)
+    v = 2 * (j.y - s)
 
     if i.y == j.y:
         result.x = (i.x + j.x) / 2
@@ -34,35 +29,30 @@ def get_intersection(breakpoint, y_sweep_line: float, max_y: float = None):
             result.y = max_y or float("inf")
             return result
 
-    elif i.y == l:
+    elif i.y == s:
         result.x = i.x
-
         p = j
-    elif j.y == l:
+
+    elif j.y == s:
         result.x = j.x
 
     else:
-        x = -(
-            np.sqrt(
-                v * (a**2 * u - 2 * a * c * u + b**2 * (u - v) + c**2 * u)
-                + d**2 * u * (v - u)
-                + l**2 * (u - v) ** 2
-            )
-            + a * v
-            - c * u
-        ) / (u - v)
+        x = -(np.sqrt(v * (i.x**2 * u - 2 * i.x * j.x * u + i.y**2 *
+                           (u - v) + j.x**2 * u) + j.y**2 * u *
+                      (v - u) + s**2 *
+                      (u - v)**2) + i.x * v - j.x * u) / (u - v)
         result.x = x
 
-    a = p.x
-    b = p.y
+    i.x = p.x
+    i.y = p.y
     x = result.x
-    u = 2 * (b - l)
+    u = 2 * (i.y - s)
 
     if u == 0:
         result.y = float("inf")
         return result
 
-    result.y = 1 / u * (x**2 - 2 * a * x + a**2 + b**2 - l**2)
+    result.y = 1 / u * (x**2 - 2 * i.x * x + i.x**2 + i.y**2 - s**2)
 
     return result
 
@@ -73,10 +63,10 @@ def calculate_angle(point, center):
     return np.math.degrees(np.math.atan2(dy, dx)) % 360
 
 
-def check_clockwise(a, b, c, center):
-    angle_1 = calculate_angle(a, center)
-    angle_2 = calculate_angle(b, center)
-    angle_3 = calculate_angle(c, center)
+def check_clockwise(x, y, z, center):
+    angle_1 = calculate_angle(x, center)
+    angle_2 = calculate_angle(y, center)
+    angle_3 = calculate_angle(z, center)
 
     counter_clockwise = (angle_3 - angle_1) % 360 > (angle_3 - angle_2) % 360
 
@@ -94,14 +84,13 @@ def _finish_edge(edge, bounding_box):
         return
 
     breakpoint = edge.get_origin().get_breakpoint()
-    edge.set_origin(
-        Vertex(breakpoint.get_coords(bounding_box.y_min - OFFSET))
-    ) if not starts and breakpoint else np.nan
+    edge.set_origin(Vertex(breakpoint.get_coords(
+        bounding_box.y_min - OFFSET))) if not starts and breakpoint else np.nan
 
     breakpoint = edge.twin.get_origin().get_breakpoint()
-    edge.set_origin(
-        Vertex(breakpoint.get_coords(bounding_box.y_min - OFFSET))
-    ) if not ends and breakpoint else np.nan
+    edge.set_origin(Vertex(
+        breakpoint.get_coords(bounding_box.y_min -
+                              OFFSET))) if not ends and breakpoint else np.nan
 
     return
 
@@ -109,13 +98,5 @@ def _finish_edge(edge, bounding_box):
 def finish_edges(edges, bounding_box):
     for edge in edges.copy():
         if not edge.get_origin().is_defined() or not bounding_box.contains(
-            [edge.get_origin()]
-        ):
+            [edge.get_origin()]):
             _finish_edge(edge, bounding_box)
-
-        # if (
-        #     not edge.get_origin().is_defined()
-        #     or not edge.twin.get_origin().is_defined()
-        # ):
-        #     edges.remove(edge)
-        #     edges.remove(edge.twin)
